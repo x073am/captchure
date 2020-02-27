eb3480eba8fa3f3770a9603f2c6a594f38dd710b
x073am <x073am@gmail.com>
2020-02-27 11:38:35 +0300

    Finish

---
+++ src/myCaptcha.php
@@ -0,0 +1,34 @@
+<?php 
+namespace Captchure;
+    class MyCaptcha{
+
+      private function getStr(){
+        $string = '';
+    
+        for($i=0;$i<4;$i++){
+          $arr = [[48,57],[65,90],[97,122]];
+          $arrRnd = array_rand($arr,1);
+          $a = $arr[$arrRnd];
+          $string .= chr(rand($a[0],$a[1]));
+        }
+        return $string;
+      }
+
+      private function imgSaver(){
+        $image = imagecreate(100, 30);
+        $white = imagecolorallocate($image, 255, 255, 255);
+        $color = imagecolorallocate($image, 200, 100, 90); // red
+        $rndm = self::getStr();
+        imagestring($image, 5, 30, 5, $rndm, $color);
+        $dir = dirname(__DIR__)."/captcha/$rndm.png";
+        imagepng($image,$dir);
+        imagedestroy($image);
+
+        return $rndm;
+      }
+
+      public static function get(){
+        return self::imgSaver();
+      }
+    }
+?>
